65d70aab12edb2b69776330d3f1c3aa047e11ccf
smdeile <smdeile@gmail.com>
2020-05-02 17:17:51 +0300

    Updates

---
+++ precache-manifest.29a7a32a5a5612cdca8344938b71f50e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a03668e8d94f24d59e64eb5d07cbca4",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "840034d05395e3d69e50",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.c8fa4ddd.chunk.css"
+  },
+  {
+    "revision": "46045b316e173767f8f0",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.19b4e05c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.19b4e05c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "840034d05395e3d69e50",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.5a434d93.chunk.js"
+  },
+  {
+    "revision": "ae3a2cafd41dba4ace62",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.3da2d935.js"
+  }
+]);
